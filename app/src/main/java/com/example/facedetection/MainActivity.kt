5962057ee3d69e6568f8892f7a7d0b4393cca096
Jesselyn Huang <jesselyn.h@weefer.co.id>
2022-08-22 08:43:54 +0700

    [JH] - Add runtime permission camera

--- app/src/main/java/com/example/facedetection/MainActivity.kt
+++ app/src/main/java/com/example/facedetection/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.facedetection
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Message
-import android.os.PersistableBundle
-import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.facedetection.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -20,12 +22,51 @@ class MainActivity : AppCompatActivity() {
     private lateinit var layout: View
     private lateinit var binding: ActivityMainBinding
 
+    private val requestPermissionCamera = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        isGranted: Boolean -> if (isGranted) {
+            Log.i("Permission: ", "Granted")
+    } else {
+            Log.i("Permission: ", "Denied")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         layout = binding.mainLayout
         setContentView(view)
+        onClickPermission(view)
+    }
+
+    fun onClickPermission(view: View) {
+        when {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_granted),
+                    Snackbar.LENGTH_INDEFINITE,
+                    null
+                ) {}
+            } // end bracket first conditional
+            ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA) -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_required),
+                    Snackbar.LENGTH_INDEFINITE,
+                    getString(R.string.ok)
+                ) {
+                    requestPermissionCamera.launch(
+                        android.Manifest.permission.CAMERA
+                    )
+                }
+            }
+            else -> {
+                requestPermissionCamera.launch(
+                    android.Manifest.permission.CAMERA
+                )
+            }
+        }
     }
 }
 // fun --> function in kotlin
